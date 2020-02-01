8e3313d70acf1a44f2878c5b4bf11c0d3d5ed701
Vanessa Dean <vanessa.dean@gmail.com>
2020-02-01 12:46:58 -0500

    Upgrade ruby and configs

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,13 @@ configure do
   use OmniAuth::Builder do
     provider :twitter, ENV['API_KEY'], ENV['API_SECRET_KEY']
   end
+end
 
+configure :development do
+  set :database, { adapter: 'postgresql', host: 'localhost', database: 'postgres' }
+end
+
+configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/database')
 
   ActiveRecord::Base.establish_connection(
